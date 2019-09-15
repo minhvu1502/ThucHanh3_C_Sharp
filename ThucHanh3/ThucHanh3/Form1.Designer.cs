bacbf4cc27546e817c08a5fd2777d0086d5ac371
minhvu1502 <43747199+minhvu1502@users.noreply.github.com>
2019-09-15 22:25:17 +0700

    Add Bài 1 Buổi Thực Hành 3

---
+++ ThucHanh3/ThucHanh3/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ThucHanh3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cb_Disk = new System.Windows.Forms.ComboBox();
+            this.cb_File = new System.Windows.Forms.ComboBox();
+            this.lb_BaiHat = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.Rtxt_Sub = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cb_Disk
+            // 
+            this.cb_Disk.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_Disk.FormattingEnabled = true;
+            this.cb_Disk.Location = new System.Drawing.Point(77, 12);
+            this.cb_Disk.Name = "cb_Disk";
+            this.cb_Disk.Size = new System.Drawing.Size(329, 21);
+            this.cb_Disk.TabIndex = 0;
+            this.cb_Disk.SelectedIndexChanged += new System.EventHandler(this.cb_Disk_SelectedIndexChanged);
+            // 
+            // cb_File
+            // 
+            this.cb_File.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_File.FormattingEnabled = true;
+            this.cb_File.Location = new System.Drawing.Point(77, 65);
+            this.cb_File.Name = "cb_File";
+            this.cb_File.Size = new System.Drawing.Size(329, 21);
+            this.cb_File.TabIndex = 1;
+            this.cb_File.SelectedIndexChanged += new System.EventHandler(this.cb_File_SelectedIndexChanged);
+            // 
+            // lb_BaiHat
+            // 
+            this.lb_BaiHat.FormattingEnabled = true;
+            this.lb_BaiHat.Location = new System.Drawing.Point(41, 132);
+            this.lb_BaiHat.Name = "lb_BaiHat";
+            this.lb_BaiHat.Size = new System.Drawing.Size(365, 173);
+            this.lb_BaiHat.TabIndex = 2;
+            this.lb_BaiHat.SelectedIndexChanged += new System.EventHandler(this.lb_BaiHat_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ổ Đĩa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Thư mục";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tập Tin";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(41, 311);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(365, 228);
+            this.axWindowsMediaPlayer1.TabIndex = 7;
+            // 
+            // Rtxt_Sub
+            // 
+            this.Rtxt_Sub.Location = new System.Drawing.Point(412, 15);
+            this.Rtxt_Sub.Name = "Rtxt_Sub";
+            this.Rtxt_Sub.Size = new System.Drawing.Size(312, 527);
+            this.Rtxt_Sub.TabIndex = 8;
+            this.Rtxt_Sub.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 551);
+            this.Controls.Add(this.Rtxt_Sub);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb_BaiHat);
+            this.Controls.Add(this.cb_File);
+            this.Controls.Add(this.cb_Disk);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chương trình nghe nhạc";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_Disk;
+        private System.Windows.Forms.ComboBox cb_File;
+        private System.Windows.Forms.ListBox lb_BaiHat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.RichTextBox Rtxt_Sub;
+    }
+}
+
---
+++ ThucHanh3/ThucHanh3/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThucHanh3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo item in drives)
+            {
+                cb_Disk.Items.Add(item.Name);
+            }
+        }
+
+        private void cb_Disk_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cb_File.Items.Clear();
+            if (cb_Disk.SelectedIndex == -1)
+            {
+                MessageBox.Show("Bạn hãy chọn thư mục");
+            }
+            else if (cb_Disk.SelectedIndex == 0)
+            {
+                DirectoryInfo Directory = new DirectoryInfo("C:\\");
+                DirectoryInfo[] Directories = Directory.GetDirectories("*.*");
+                FileInfo[] File = Directory.GetFiles();
+                foreach (DirectoryInfo item in Directories)
+                {
+                    cb_File.Items.Add(item.Name);
+                }
+            }
+            else if (cb_Disk.SelectedIndex == 1)
+            {
+                DirectoryInfo Directory = new DirectoryInfo("D:\\");
+                DirectoryInfo[] Directories = Directory.GetDirectories("*.*");
+                FileInfo[] File = Directory.GetFiles();
+                foreach (DirectoryInfo item in Directories)
+                {
+                    cb_File.Items.Add(item.Name);
+                }
+            }
+            else if (cb_Disk.SelectedIndex == 2)
+            {
+                DirectoryInfo Directory = new DirectoryInfo("E:\\");
+                DirectoryInfo[] Directories = Directory.GetDirectories("*.*");
+                FileInfo[] File = Directory.GetFiles();
+                foreach (DirectoryInfo item in Directories)
+                {
+                    cb_File.Items.Add(item.Name);
+                }
+            }
+            else if (cb_Disk.SelectedIndex == 3)
+            {
+                DirectoryInfo Directory = new DirectoryInfo("F:\\");
+                DirectoryInfo[] Directories = Directory.GetDirectories("*.*");
+                FileInfo[] File = Directory.GetFiles();
+                foreach (DirectoryInfo item in Directories)
+                {
+                    cb_File.Items.Add(item.Name);
+                }
+            }
+        }
+
+        private void cb_File_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lb_BaiHat.Items.Clear();
+            Rtxt_Sub.Text = "";
+            string[] files = Directory.GetFiles(cb_Disk.Text + cb_File.Text);
+            foreach (string item in files)
+            {
+                lb_BaiHat.Items.Add(item);
+            }
+        }
+
+        private void lb_BaiHat_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lb_BaiHat.SelectedIndex == -1)
+            {
+                MessageBox.Show("Hãy chọn 1 bài hát");
+            }
+            else
+            {
+                string check = ".txt";
+                if (lb_BaiHat.SelectedItem.ToString().Contains(check) == true)
+                {
+                    MessageBox.Show("Hãy chọn định dạng MP3");
+                }
+                else
+                {
+                    Rtxt_Sub.Text = "";
+                    string filePath = lb_BaiHat.SelectedItem.ToString() + "_Lyric.txt";
+                    if (File.Exists(filePath) == true)
+                    {
+                        FileStream fs = new FileStream(filePath, FileMode.Open);
+                        StreamReader rd = new StreamReader(fs, Encoding.UTF8);
+                        string loibaihat = rd.ReadToEnd();
+                        Rtxt_Sub.Text = loibaihat;
+                    }
+                    else if (File.Exists(filePath) == false)
+                    {
+                        Rtxt_Sub.Text = "Không có lời bài hát";
+                    }
+                    axWindowsMediaPlayer1.URL = lb_BaiHat.SelectedItem.ToString();
+                }
+            }
+        }
+    }
+}
